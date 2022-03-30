a5d0bf4dab789441cd5d2d6ee965622960a8d981
Pavlos Vinieratos <pvinis@gmail.com>
2022-03-30 02:03:30 +0100

    done for the day

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 400 × 240 pixels
 lua
 
-- logo drop
+- logo drop (try cranking!)
+- get images from artsy.net
+- get images from https://api.artsy.net/api/v1/artwork/kunstrasen-art-rodeo-4/default_image/medium_rectangle.jpg, put them in support/artworks
+- run ./scripts/dither-artworks
+
+
+- add some data for the artwork?
 
 
 run/dev:
--- source/main.lua
+++ source/main.lua
@@ -7,58 +7,137 @@ import "CoreLibs/timer"
 import "CoreLibs/crank"
 import "CoreLibs/ui"
 
-
+local kModeLogoDrop, kModeMain = 0, 1
+gameMode = kModeMain
 
 local menu = playdate.getSystemMenu()
 local menuItem, error = menu:addMenuItem("Lol", function()
-print("lol indeed")
+	print("lol indeed")
 end)
 
-local gfx <const> = playdate.graphics -- shorthand
-
-function setupLogoDrop()
-    playdate.ui.crankIndicator:start()
-    -- playdate.ui.crankIndicator:update()
-    -- show this a bit later than the logo drop
-
-    local logoImage = gfx.image.new("images/cropped")
-    assert(logoImage)
-
-    logoSprite = gfx.sprite.new(logoImage)
-    logoSprite:moveTo(200, -60)
-    logoSprite:add()
-
-    dling = playdate.sound.sampleplayer.new("sounds/dling")
-
+local gfx<const> = playdate.graphics -- shorthand
+function tprint(tbl, indent)
+	if not indent then
+		indent = 0
+	end
+	for k, v in pairs(tbl) do
+		formatting = string.rep("  ", indent) .. k .. ": "
+		if type(v) == "table" then
+			print(formatting)
+			tprint(v, indent + 1)
+		elseif type(v) == "boolean" then
+			print(formatting .. tostring(v))
+		else
+			print(formatting .. v)
+		end
+	end
 end
 
-setupLogoDrop()
+function tableLength(tbl)
+	count = 0
+	for _ in pairs(tbl) do
+		count = count + 1
+	end
+	return count
+end
+
+function setupLogoDrop()
+	playdate.ui.crankIndicator:start()
+	-- playdate.ui.crankIndicator:update()
+	-- show this a bit later than the logo drop
+
+	local logoImage = gfx.image.new("images/cropped")
+	assert(logoImage)
+
+	logoSprite = gfx.sprite.new(logoImage)
+	logoSprite:moveTo(200, -60)
+	logoSprite:add()
+
+	dling = playdate.sound.sampleplayer.new("sounds/dling")
+end
+
+artworkIndex = 1
+artworks = playdate.file.listFiles("images/artworks/")
+artworksLength = tableLength(artworks)
+scale = 1
+
+function changeArtwork(diff)
+	artworkIndex = artworkIndex + diff
+	if artworkIndex > artworksLength then
+		artworkIndex = 1
+	end
+	if artworkIndex < 1 then
+		artworkIndex = artworksLength
+	end
+	print(artworkIndex)
+	scale = 1
+
+	local artworkImage = gfx.image.new("images/artworks/" .. artworks[artworkIndex])
+	assert(artworkImage)
+
+	if artworkSprite then
+		artworkSprite:setImage(artworkImage)
+	else
+		artworkSprite = gfx.sprite.new(artworkImage)
+	end
+	artworkSprite:setScale(scale)
+	artworkSprite:moveTo(200, 120)
+	artworkSprite:add()
+end
+
+function setupMain()
+	changeArtwork(1)
+end
+
+-- setupLogoDrop()
+
+setupMain()
 
 function playdate.update()
-    -- move logo lower
-    local newY = logoSprite.y + 1
-    print("newY", newY)
-    if newY > 120 then
-        -- logoSprite:remove()
-    else
-        logoSprite:moveBy(0, 2)
-        if newY == 111 then
-            dling:play()
-        end
-    end
+	if gameMode == kModeLogoDrop then
+		-- move logo lower
+		local newY = logoSprite.y + 1
+		print("newY", newY)
+		if newY > 120 then
+			-- logoSprite:remove()
+		else
+			logoSprite:moveBy(0, 2)
+			if newY == 111 then
+				dling:play()
+			end
+		end
+	elseif gameMode == kModeMain then
+		if playdate.buttonIsPressed("down") then
+			artworkSprite:moveBy(0, 2)
+		elseif playdate.buttonIsPressed("up") then
+			artworkSprite:moveBy(0, -2)
+		end
+		if playdate.buttonJustPressed("right") then
+			changeArtwork(1)
+		elseif playdate.buttonJustPressed("left") then
+			changeArtwork(-1)
+		end
+	end
 
-
-    -- housekeeping
-    gfx.sprite.update()
-    playdate.timer.updateTimers()
+	-- housekeeping
+	gfx.sprite.update()
+	playdate.timer.updateTimers()
 end
 
 function playdate.cranked(change, acceleratedChange)
-    logoSprite:moveBy(change, 0)
+	if gameMode == kModeLogoDrop then
+		logoSprite:moveBy(change, 0)
+	elseif gameMode == kModeMain then
+		scale = scale + change / 100
+		if scale < 0.01 then
+			scale = 0.01
+		end
+		artworkSprite:setScale(scale)
+		print(scale)
+	end
 end
 
-
 function playdate.gameWillPause()
-	local img = gfx.image.new('images/woop')
+	local img = gfx.image.new("images/woop")
 	playdate.setMenuImage(img)
 end
